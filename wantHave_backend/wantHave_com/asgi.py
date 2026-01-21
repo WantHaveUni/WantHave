1b509458b41ba819f27119111a66a602a3c8d51b
Nikola Vasic <nikola.vasic@edu.fh-joanneum.at>
2026-01-21 16:58:21 +0100

    Chatty

--- wantHave_backend/wantHave_com/asgi.py
+++ wantHave_backend/wantHave_com/asgi.py
@@ -8,13 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+# CRITICAL: Set settings BEFORE importing any Django apps/models
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wantHave_com.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wantHave_com.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
